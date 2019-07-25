5f32164728db70d993443235ed68f8844547edf3
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-25 14:41:49 +0300

    Updates

---
+++ precache-manifest.f5f000735b6e6e13ccca8e64cd95f999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbb3fdbcabde95da689451cdfb4721c",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "b6530aea671a834d273f",
+    "url": "/react_add-todo-form/static/css/main.4bbd3aea.chunk.css"
+  },
+  {
+    "revision": "04d3a49fe037833a0b63",
+    "url": "/react_add-todo-form/static/js/2.97420267.chunk.js"
+  },
+  {
+    "revision": "b6530aea671a834d273f",
+    "url": "/react_add-todo-form/static/js/main.7df8fb7a.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
